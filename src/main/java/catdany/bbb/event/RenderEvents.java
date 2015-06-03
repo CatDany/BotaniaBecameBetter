fbbb67ffb241a68637622fc7132b52ea7d72744a
Dany <dany2001ru@gmail.com>
2015-06-03 22:10:20 +0600

    Fixed derps

--- src/main/java/catdany/bbb/event/RenderEvents.java
+++ src/main/java/catdany/bbb/event/RenderEvents.java
@@ -1,11 +1,11 @@
 package catdany.bbb.event;
 
-import vazkii.botania.common.Botania;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.InventoryEffectRenderer;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.RenderPlayerEvent;
-import baubles.client.gui.GuiPlayerExpanded;
+import vazkii.botania.common.Botania;
+import catdany.bbb.Log;
 import catdany.bbb.Refs;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
@@ -22,15 +22,16 @@ public class RenderEvents
 	{
 		if (!e.entityPlayer.getHideCape() && !(Minecraft.getMinecraft().currentScreen instanceof InventoryEffectRenderer) && e.entityPlayer.getGameProfile().getId().toString().equals(Refs.T3H_31337_UUID))
 		{
-			int t = (int)(e.entity.ticksExisted % 60);
+			int t = (int)(e.entityPlayer.ticksExisted % 60);
 			int a = (int)(t / 60D * 360);
 			if (a != lastAngle)
 			{
 				lastAngle = a;
-				double pX = e.entity.posX + Math.sin(Math.toRadians(a)) * 0.85;
-				double pY = e.entity.posY - 0.5;
-				double pZ = e.entity.posZ + Math.cos(Math.toRadians(a)) * 0.85;
-				World world = e.entity.worldObj;
+				boolean f = Minecraft.getMinecraft().thePlayer.getCommandSenderName().equals("CatDany");
+				double pX = e.entityPlayer.posX + Math.sin(Math.toRadians(a)) * 0.85;
+				double pY = e.entityPlayer.posY - 0.5 + (f ? 0 : 1.62);
+				double pZ = e.entityPlayer.posZ + Math.cos(Math.toRadians(a)) * 0.85;
+				World world = e.entityPlayer.worldObj;
 				Botania.proxy.wispFX(world, pX, pY, pZ, 0.4F, 0.4F, 1.0F, 0.15F, 0.0F, 1.0F);
 				//world.spawnParticle("happyVillager", pX, pY, pZ, 0, 0, 0);
 			}
